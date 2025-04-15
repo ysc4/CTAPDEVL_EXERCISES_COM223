4e9daff06079cde3c1383ae5dcf13a38f63a3414
Jessy Cassandra <111488139+ysc4@users.noreply.github.com>
2025-04-15 18:39:39 +0800

    Create MainActivity.kt

---
+++ exercise3/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.exercise2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private val validUsername = "username01"
+    private val validPassword = "name@123"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val usernameInput = findViewById<EditText>(R.id.username)
+        val passwordInput = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val enteredUsername = usernameInput.text.toString()
+            val enteredPassword = passwordInput.text.toString()
+
+            if (enteredUsername == validUsername && enteredPassword == validPassword) {
+                val intent = Intent(this, Homepage::class.java)
+                intent.putExtra("USERNAME", enteredUsername)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Invalid credentials", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
